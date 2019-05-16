6bfdee11568f5145c0ebacc6f6bc72d5e1ed85e6
Alex <alexiv1102@gmail.com>
2019-05-16 09:50:11 +0300

    Updates

---
+++ precache-manifest.291c524579b229c5329d8b095a07b7c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b09967df3ed70c762d42c28ef393535",
+    "url": "/user-interface-for-receiving-payments-react/index.html"
+  },
+  {
+    "revision": "e2aa7c7d22667587c186",
+    "url": "/user-interface-for-receiving-payments-react/static/css/main.faa81318.chunk.css"
+  },
+  {
+    "revision": "a149c2d3c3f8b10e9dbd",
+    "url": "/user-interface-for-receiving-payments-react/static/js/2.213308ea.chunk.js"
+  },
+  {
+    "revision": "e2aa7c7d22667587c186",
+    "url": "/user-interface-for-receiving-payments-react/static/js/main.7b7b29b1.chunk.js"
+  },
+  {
+    "revision": "542c65cd8e0f6d648a24",
+    "url": "/user-interface-for-receiving-payments-react/static/js/runtime~main.2e668586.js"
+  },
+  {
+    "revision": "f61b3e8126785a40cdabb2e2f30b1adc",
+    "url": "/user-interface-for-receiving-payments-react/static/media/paypal-logo.f61b3e81.png"
+  },
+  {
+    "revision": "2b109fdf4d6a8ed598e000a0668d31d2",
+    "url": "/user-interface-for-receiving-payments-react/static/media/visa-and-mastercard-logos.2b109fdf.png"
+  }
+]);
